91b8d2a19c4faedbb3dab83e1b5010445448d8ce
WilhamsGomes <wilhamsgoomes@gmail.com>
2021-11-04 21:12:38 -0300

    Adicionando Home

---
+++ src/Components/Valores/valores.js
@@ -0,0 +1,33 @@
+import React from "react";
+import './valores.css'
+
+export default function Valores(){
+    return(
+        <div className="valores">
+            <div>
+
+                <h3>+ 50 MIL</h3>
+                <hr></hr>
+                <p>Produtos vendidos</p>
+                
+            </div>
+
+            <div>
+
+                <h3>+ 20 MIL</h3>
+                <hr></hr>
+                <p>Clientes atendidos</p>
+                
+            </div>
+
+            <div>
+
+                <h3>+ 2 MIL</h3>
+                <hr></hr>
+                <p>Cidades atendidas</p>
+
+            </div>
+
+        </div>
+    );
+};
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Header from '../../Components/Header';
+import Cards from '../../Components/Cards/Cards';
+import Valores from '../../Components/Valores/valores';
+
+import './index.css'
 
 export default function Home (){
   return (
     <div>
-        <Header/>
-        Home
+        <Cards/>
+        {/*<div className="publi">
+            <h1>Start sua beleza, trazendo mais vida para seu dia a dia</h1>
+        </div>*/}
+        <Valores/>
     </div>
   );
 }
