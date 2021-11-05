88f172a477693689291f5699871ab3a18434752c
WilhamsGomes <wilhamsgoomes@gmail.com>
2021-11-05 13:51:38 -0300

    Adicionando carrosel

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from '../../Components/Header';
 import Cards from '../../Components/Cards/Cards';
 import Valores from '../../Components/Valores/valores';
+import Navbar from '../../Components/Navbar';
+import App from '../../Components/Carrossel/Carrossel';
 
 import './index.css'
 
@@ -9,10 +11,13 @@ export default function Home() {
   return (
     <div>
 
+
+      <Navbar/>
+      <App/>
       <Cards/>
         <div className="publi">
             <h1>Start sua beleza, trazendo mais vida para seu dia a dia</h1>
-        </div>*/
+        </div>
       <Valores/>
 
     </div>
