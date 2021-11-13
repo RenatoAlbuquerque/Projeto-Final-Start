772a92a1d80475824579517c6da60ad2ecbae182
Renato Albuquerque <renatoviniciusabreu.2021@gmail.com>
2021-11-13 19:34:16 -0300

    Update Bag.jsx

--- src/Components/Bag/Bag.jsx
+++ src/Components/Bag/Bag.jsx
@@ -1,15 +1,15 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import bag3 from '../../Images/IconCompras/bag3.png';
 import bag2 from '../../Images/IconCompras/bag2.png';
 
 export default function Bag(props) {
     const [bag, setBag] = useState()
 
-    useEffect(() =>{
+    useEffect(() => {
         const itens = localStorage.getItem('itens')
-        if(!itens){
+        if (!itens) {
             setBag(bag3)
-        }else{
+        } else {
             setBag(bag2)
         }
         console.log('asdas')
