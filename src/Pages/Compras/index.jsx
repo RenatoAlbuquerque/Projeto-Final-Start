b41f4e29a4e5ad1944bc24aaeaa5dcf064163e9e
gilbertomoj <gibamedeirosgc@gmail.com>
2021-11-14 13:30:54 -0300

    Adicionando modais

--- src/Pages/Compras/index.jsx
+++ src/Pages/Compras/index.jsx
@@ -20,7 +20,32 @@ export default function Compras() {
   //     const login = localStorage.getItem('login')
   //     setUser(login)
   // },[])
+function ItemComponent(props){
+  if(itens){
+    return(
+      <div className="item-list">
+      {
+        itens.map( (element, key) => {
+          return(
+            <div className="item">
+            <a onClick={()=>{deleteItem(element.nome)}}><img src={Bin} alt="Bin icon" className="bin-icon"/></a>
+            <Items nome={element.nome} preco={element.preco} quant={element.quant} marca={element.marca} img={element.img}/>
+  
+            </div>
+          )
+        })
+      }
+      <button className="end-shop" onClick={()=>{endShop()}}>Finalizar Compra</button>
+  
+      </div>
+    )
+  }else{
+    return(
+    <div><h1 className="message">Sacola Vazia</h1></div>
+    )
+  }
 
+}
   
   useEffect(() => {
     let produtos = JSON.parse(localStorage.getItem('itens'))
@@ -57,45 +82,65 @@ export default function Compras() {
 
  function endShop(){
   Swal.fire({
-    title:'Compra finalizada',
-    text: `Itens comprados`,
+    titleText: "Itens comprados",
+    html: showItens(),
     icon: 'info',
+    width: 600,
     showDenyButton: true,
     denyButtonText: 'Não',
     confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
+    cancelButtonColor: '#3085d6',
     confirmButtonText: 'Sim'
+  }).then((result) => {
+    /* Read more about isConfirmed, isDenied below */
+    if (result.isConfirmed) {
+      Swal.fire('Saved!', '', 'success')
+    } else if (result.isDenied) {
+      Swal.fire('Sua compra foi finalizada', '', 'info')
+      localStorage.removeItem("itens");
+      setItens()
+    }
   })
+
  }
 
- function setPlus(element){
-   
+ function showItens(){
+  let produtos = JSON.parse(localStorage.getItem('itens'))
+  var string = ''
+  var finalV = 0
+  for(var i in produtos){
+    string += produtos[i].nome + "  " + produtos[i].quant + " un. R$" + produtos[i].preco * produtos[i].quant +'<br>'
+    finalV += produtos[i].preco * produtos[i].quant
+  }
+
+  string += "<b>Valor da Compra : R$ " + finalV + "</b><br>"
+  string += "Compra efetuada pelo usuário \"<b>" + user + "</b>\"<br><br><br>"
+  string += "Deseja gerar o PDF da compra ?"
+  console.log(string)
+  return string
 }
 
 
   return (
     <div className="bag-container">
       <Header />
-      <h1>Sacola de Compras</h1>
-      <div className="item-list">
+      {/* <h1>Sacola de Compras</h1> */}
+      { ItemComponent()}
+      {/* <div className="item-list">
       {
         itens.map( (element, key) => {
           return(
             <div className="item">
             <a onClick={()=>{deleteItem(element.nome)}}><img src={Bin} alt="Bin icon" className="bin-icon"/></a>
             <Items nome={element.nome} preco={element.preco} quant={element.quant} marca={element.marca} img={element.img}/>
-            {/* <a onClick={()=>{}}>+</a>
-            {element.quant}
-            <a onClick={()=>{}}>-</a> */}
-            {/* <p>Valor total</p>
-            <h1>R$ {element.quant * element.preco}</h1> */}
+
             </div>
           )
         })
       }
       <button className="end-shop" onClick={()=>{endShop()}}>Finalizar Compra</button>
 
-      </div>
+      </div> */}
       <Footer />
     </div>
   );
